59fe494eafa3e4a9dbef27e95dc10cd2eb4a4e8b
Channie Duffman <channieduffman@gmail.com>
2023-08-13 00:16:42 -0500

    first commit - create app factory

---
+++ basicb/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask
+
+
+def create_app(test_config=None):
+    # establish instance of Flask
+    app = Flask(__name__, instance_relative_config=True)
+    # set default config values
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'basicb.sqlite'),
+    )
+
+    # check for test configuration
+    if test_config is None:
+        # load config.py is not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # else load test config
+        app.config.from_mapping(test_config)
+
+    # ensure existence of instance folder
+    try:
+        os.makedirs(app.instance_path)
+    except OSError: # thrown if already exists
+        pass
+
+    # default test route
+    @app.route('/hello')
+    def hello():
+        return 'Hello, world!'
+    
+    return app
